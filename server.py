79e890e22634377cb08821c23f8f4ef4be813959
Hermalez <52384310+Hermalez@users.noreply.github.com>
2019-10-19 16:00:27 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,19 @@ from flask import Flask
 import os
 from flask import render_template
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://hackershelping:hackfsu@hackershelping-akpvf.gcp.mongodb.net/test?retryWrites=true&w=majority')
+
+def my_function(mydb):
+    db = client.get_database(mydb)
+    return db.collection.find().count()
+
+print(my_function('my_database'))
+
+#mongodb+srv://hackershelping:hackfsu@hackershelping-akpvf.gcp.mongodb.net/test?retryWrites=true&w=majority
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
