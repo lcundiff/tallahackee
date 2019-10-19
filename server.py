4d7e18fb94e0b3d2aeed474a3be23fcc414ce174
Logan Cundiff <lcundiff@ufl.edu>
2019-10-19 14:18:43 -0400

    good start

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+from flask import Flask
+import os
+from flask import render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():    
   return render_template('login.html') #this is the home page currently
